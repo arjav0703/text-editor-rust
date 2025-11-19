64b126e02f35685eceee0a22b4137f67bb5c44f6
Arjav Jain <arjavjain0703@gmail.com>
2025-11-19 18:46:38 +0530

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use std::io::{self, Read, Write};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{self, Read};
 
 fn main() {
-    for c in io::stdin().bytes() {
-        let c = c.unwrap() as char;
-        println!("{}", c);
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+
+                if c == 'q' {
+                    disable_raw_mode().unwrap();
+                    break;
+                }
+            }
+            Err(e) => println!("Error: {}", e),
+        }
     }
 }
