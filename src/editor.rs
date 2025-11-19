2a35992cf5f3c1ec1cb47cd089e1c483ebbaeaf6
Arjav Jain <arjavjain0703@gmail.com>
2025-11-19 21:48:45 +0530

    refactor

--- src/editor.rs
+++ src/editor.rs
@@ -55,13 +55,13 @@ impl Editor {
             println!("byeeeeee")
         } else {
             self.draw_tildes()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Coords::index())?;
         }
         Ok(())
     }
 
     fn draw_tildes(&self) -> Result<()> {
-        let (height, _width) = Terminal::size()?;
+        let Size { height, width: _w } = Terminal::size()?;
         for current_row in 0..height {
             print!("~");
             if current_row + 1 < height {
@@ -72,17 +72,36 @@ impl Editor {
     }
 }
 
+pub struct Size {
+    pub height: u16,
+    pub width: u16,
+}
+
+pub struct Coords {
+    pub x: u16,
+    pub y: u16,
+}
+
+impl Coords {
+    pub fn index() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
 struct Terminal {}
 impl Terminal {
     pub fn clear_screen() -> Result<()> {
         execute!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<()> {
+    pub fn move_cursor_to(position: Coords) -> Result<()> {
+        let Coords { x, y } = position;
         execute!(stdout(), MoveTo(x, y))?;
         Ok(())
     }
-    pub fn size() -> Result<(u16, u16)> {
-        Ok(size()?)
+    pub fn size() -> Result<Size> {
+        let (width, height) = size()?;
+        let size = Size { height, width };
+        Ok(size)
     }
 }
