9b76326c89cf42f2664ec94e3bb7f748e57aafe7
Arjav Jain <arjavjain0703@gmail.com>
2025-11-20 16:20:22 +0530

    make the cursor move

--- src/editor.rs
+++ src/editor.rs
@@ -1,22 +1,30 @@
 use anyhow::Result;
+use core::cmp::min;
 use crossterm::{
-    cursor::MoveTo,
-    cursor::{Hide, Show},
-    event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read},
+    cursor::{Hide, MoveTo, Show},
+    event::{
+        Event::{self, Key},
+        KeyCode::{self, Char},
+        KeyEvent, KeyModifiers, read,
+    },
     execute, queue,
     style::Print,
     terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size},
 };
-use std::io::stdout;
+use std::io::{Write, stdout};
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct Editor {
     running: bool,
+    position: Coords,
 }
 
 impl Editor {
     pub fn new() -> Self {
-        Self { running: true }
+        Self {
+            running: true,
+            position: Coords::index(),
+        }
     }
 
     pub fn run(&mut self) -> Result<()> {
@@ -31,13 +39,13 @@ impl Editor {
             }
 
             let event = read()?;
-            self.evaluate_event(&event);
+            self.evaluate_event(&event)?;
         }
         disable_raw_mode()?;
         Ok(())
     }
 
-    fn evaluate_event(&mut self, event: &Event) {
+    fn evaluate_event(&mut self, event: &Event) -> Result<()> {
         if let Key(KeyEvent {
             code, modifiers, ..
         }) = event
@@ -46,22 +54,31 @@ impl Editor {
                 Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.running = false;
                 }
-                _ => (),
+                KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right => {
+                    self.move_point(*code)?;
+                }
+                _ => {}
             }
         }
+        Ok(())
     }
 
     fn refresh_screen(&self) -> Result<()> {
         Terminal::hide_cursor()?;
+        Terminal::move_cursor_to(Coords::index())?;
 
         if !self.running {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye!\n")?;
         } else {
             self.draw_tildes()?;
-            Terminal::move_cursor_to(Coords::index())?;
+            Terminal::move_cursor_to(Coords {
+                x: self.position.x,
+                y: self.position.y,
+            })?;
         }
         Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
@@ -76,6 +93,30 @@ impl Editor {
         }
         Ok(())
     }
+
+    fn move_point(&mut self, keycode: KeyCode) -> Result<()> {
+        let Coords { mut x, mut y } = self.position;
+        let Size { height, width } = Terminal::size()?;
+
+        match keycode {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            _ => {}
+        }
+
+        self.position = Coords { x, y };
+        Ok(())
+    }
 }
 
 pub struct Size {
@@ -83,6 +124,7 @@ pub struct Size {
     pub width: u16,
 }
 
+#[derive(Debug, Clone)]
 pub struct Coords {
     pub x: u16,
     pub y: u16,
@@ -126,6 +168,10 @@ impl Terminal {
 
     pub fn print(string: &str) -> Result<()> {
         queue!(stdout(), Print(string))?;
+        Ok(())
+    }
+    pub fn execute() -> Result<()> {
+        stdout().flush()?;
         Ok(())
     }
 }
